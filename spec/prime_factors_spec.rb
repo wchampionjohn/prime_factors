8817b628f2a61e7afcbe3666cc9ecca125ca31ef
john.hsieh <john.hsieh@rockmobile.com>
2016-10-20 10:53:16 +0800

    added test case input 2 should return [2]

--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -5,4 +5,8 @@ describe 'PrimeFactors' do
   it 'should return [] if input of 1' do
     expect(PrimeFactory.of(1)).to eq []
   end
+
+  it 'should return [2] if input of 2' do
+    expect(PrimeFactory.of(2)).to eq [2]
+  end
 end
