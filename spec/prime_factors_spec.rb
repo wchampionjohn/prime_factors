197d3a5dcccc313e0b321ee0faca20232c3de764
john.hsieh <john.hsieh@rockmobile.com>
2016-10-20 11:36:03 +0800

    新增輸入值8的test case

--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -10,6 +10,7 @@ describe 'PrimeFactors' do
     [7, [7]],
     [4, [2, 2]],
     [6, [2, 3]],
+    [8, [2, 2, 2]],
   ].each do |num, expression|
     it "should return #{expression.to_s} if input of #{num.to_s}" do
       expect(PrimeFactor.of(num)).to eq expression
