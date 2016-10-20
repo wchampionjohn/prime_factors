74e03037f0c259016f88cf02a4b23d9d77a036e0
john.hsieh <john.hsieh@rockmobile.com>
2016-10-20 11:33:56 +0800

    refactor prime factors class

--- lib/prime_factors.rb
+++ lib/prime_factors.rb
@@ -4,16 +4,15 @@ require "byebug"
 module PrimeFactor
   def self.of num
     factors = []
+
     return factors if num == 1
 
     if num % 2 == 0
       factors << 2
-      num = num / 2
-
-      factors << 2 if Prime.prime? num
-    else
-      factors << num
+      num/=2
     end
+
+    factors << num if Prime.prime? num
 
     factors
   end
