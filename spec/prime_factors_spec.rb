15fed7b0b837cac696420ac4466ab0b632bc039e
john.hsieh <john.hsieh@rockmobile.com>
2016-10-20 11:05:10 +0800

    新增輸入4的test case

--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -6,7 +6,8 @@ describe 'PrimeFactors' do
     [1, []],
     [2, [2]],
     [3, [3]],
-    [5, [5]]
+    [5, [5]],
+    [4, [2, 2]],
   ].each do |num, expression|
     it "should return #{expression.to_s} if input of #{num.to_s}" do
       expect(PrimeFactor.of(num)).to eq expression
