e7f9e828515808675034b5322345059b966622ff
john.hsieh <john.hsieh@rockmobile.com>
2016-10-20 11:04:09 +0800

    refactory test case

--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -2,19 +2,14 @@ require 'prime_factors'
 require 'byebug'
 
 describe 'PrimeFactors' do
-  it 'should return [] if input of 1' do
-    expect(PrimeFactor.of(1)).to eq []
-  end
-
-  it 'should return [2] if input of 2' do
-    expect(PrimeFactor.of(2)).to eq [2]
-  end
-
-  it 'should return [3] if input of 3' do
-    expect(PrimeFactor.of(3)).to eq [3]
-  end
-
-  it 'should return [5] if input of 5' do
-    expect(PrimeFactor.of(5)).to eq [5]
+  [
+    [1, []],
+    [2, [2]],
+    [3, [3]],
+    [5, [5]]
+  ].each do |num, expression|
+    it "should return #{expression.to_s} if input of #{num.to_s}" do
+      expect(PrimeFactor.of(num)).to eq expression
+    end
   end
 end
