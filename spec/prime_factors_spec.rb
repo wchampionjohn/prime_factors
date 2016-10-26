d74736496c43c4953d34fdc7fd48cd0426601263
john.hsieh <john.hsieh@rockmobile.com>
2016-10-26 10:34:19 +0800

    新增數個數值較大的test case

--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -12,6 +12,11 @@ describe 'PrimeFactors' do
     [6, [2, 3]],
     [8, [2, 2, 2]],
     [9, [3, 3]],
+    [2*3*5*7*11*13, [2, 3, 5, 7, 11, 13]],
+    [8191*131071, [8191, 131071]],
+    [2**100, [2]*100],
+    [2**19-1, [2**19-1]],
+    [131071*(2**19-1), [131071, 2**19-1]]
   ].each do |num, expression|
     it "should return #{expression.to_s} if input of #{num.to_s}" do
       expect(PrimeFactor.of(num)).to eq expression
